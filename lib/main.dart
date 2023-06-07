deee8837de78b69f7c5ef3f28d2795294444d1e0
anjutus <anjutuscano91@gmail.com>
2023-06-07 10:44:30 -0500

    Error handing and formatting

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:recipe_creator/screen/recipe_screen.dart';
 
 void main() {
-  runApp(RecipeApp());
+  runApp(const RecipeApp());
 }
 
 class RecipeApp extends StatelessWidget {
+  const RecipeApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -13,7 +15,7 @@ class RecipeApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
         appBarTheme: const AppBarTheme(backgroundColor: Color.fromRGBO(216, 139, 110,1)),
-        elevatedButtonTheme: ElevatedButtonThemeData(style:ElevatedButton.styleFrom (backgroundColor:const Color.fromRGBO(175, 70, 87,1),shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)) )),
+        elevatedButtonTheme: ElevatedButtonThemeData(style:ElevatedButton.styleFrom (backgroundColor:const Color.fromRGBO(175, 70, 87,1),padding: const EdgeInsets.all(10),shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)) )),
         canvasColor: const Color.fromRGBO(242, 239, 234,1)
       ),
       home: RecipeScreen(),
--- lib/screen/recipe_screen.dart
+++ lib/screen/recipe_screen.dart
@@ -3,6 +3,7 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:recipe_creator/constant/openai_key.dart';
 import 'package:recipe_creator/models/openai_model.dart';
+
 String openaiApiKey = apiKey;
 
 class RecipeScreen extends StatefulWidget {
@@ -24,10 +25,14 @@ class _RecipeScreenState extends State<RecipeScreen> {
     try {
       final response = await http.post(
         Uri.parse('https://api.openai.com/v1/completions'),
-        headers: {'Content-Type': 'application/json','Authorization': 'Bearer $openaiApiKey'},
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer $openaiApiKey'
+        },
         body: json.encode({
           "model": "text-davinci-003",
-          "prompt": "Write a recipe based on these ingredients with instructions:\n\n${_ingredients}",
+          "prompt":
+              "Write a recipe based on these ingredients with instructions:\n\n${_ingredients}",
           "temperature": 0.3,
           "max_tokens": 500,
           "top_p": 1,
@@ -35,18 +40,19 @@ class _RecipeScreenState extends State<RecipeScreen> {
           "presence_penalty": 0
         }),
       );
-      String responsestatus=response.statusCode.toString();
-     // print('response $responsestatus');
-      //final Map<String, dynamic> data = json.decode(response.body);
-      final data = recipeDataFromJson(response.body);
-      //print('data $data');
-      final String recipeText = data.choices[0].text;
-      //print('recipeText $recipeText');
-      setState(() {
-        _recipe = recipeText;
-      });
+      if (response.statusCode.toString() == "200") {
+        final data = recipeDataFromJson(response.body);
+
+        final String recipeText = data.choices[0].text;
+        setState(() {
+          _recipe = recipeText;
+        });
+      } else {
+        throw http.ClientException("Open AI API not available");
+      }
     } catch (e) {
       //print('Error: $e');
+      //_recipe = "Error: $e";
     } finally {
       setState(() {
         _isLoading = false;
@@ -58,14 +64,14 @@ class _RecipeScreenState extends State<RecipeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Recipe Creator'),
+        title: const Text('Recipe Creator'),
       ),
       body: SingleChildScrollView(
-        padding: EdgeInsets.all(16.0),
+        padding: const EdgeInsets.all(16.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            SizedBox(height: 26.0),
+            const SizedBox(height: 26.0),
             TextField(
               onChanged: (value) {
                 setState(() {
@@ -73,31 +79,33 @@ class _RecipeScreenState extends State<RecipeScreen> {
                 });
               },
               decoration: InputDecoration(
-                floatingLabelStyle: TextStyle(color: Colors.black,),
-                focusedBorder:OutlineInputBorder(
+                floatingLabelStyle: const TextStyle(
+                  color: Colors.black,
+                ),
+                focusedBorder: OutlineInputBorder(
                   borderSide: const BorderSide(color: Colors.black),
                   borderRadius: BorderRadius.circular(5.0),
                 ),
-                border: OutlineInputBorder(borderSide: BorderSide(color: Colors.black),borderRadius: BorderRadius.circular(5.0) ),
+                border: OutlineInputBorder(
+                    borderSide: const BorderSide(color: Colors.black),
+                    borderRadius: BorderRadius.circular(5.0)),
                 labelText: 'Enter Ingredients',
               ),
             ),
-            SizedBox(height: 16.0),
+            const SizedBox(height: 16.0),
             ElevatedButton(
               onPressed: _isLoading ? null : _generateRecipe,
-
               child: _isLoading
-                  ? CircularProgressIndicator()
-                  : Text('Generate Recipe'),
+                  ? const CircularProgressIndicator()
+                  : const Text('Generate Recipe'),
             ),
-            SizedBox(height: 16.0),
+            const SizedBox(height: 16.0),
             // if(_isLoading && _recipe!='')
             // Card(color:const Color.fromRGBO(255, 255, 255, 1),margin: EdgeInsets.all(8),elevation: 2,shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),child:
-        Text(
+            Text(
               _recipe,
-              style: TextStyle(fontSize: 16.0),
-            ) ,
-            
+              style: const TextStyle(fontSize: 16.0),
+            ),
           ],
         ),
       ),
